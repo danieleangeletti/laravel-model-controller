8608aa93eb2d3ba771ab1421443a3129e140ee90
danieleangeletti <angelettidaniele@gmail.com>
2024-02-26 15:58:19 +0100

    inizializzazione progetto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,27 +19,9 @@
         @include('partials.header')
 
         <main>
-            <div class="container">
 
-                <div class="row">
+            @yield('main-content')
 
-                    <div class="col">
-
-                        <div>
-                            <img class="gatto-img" src="{{ Vite::asset('resources/img/gatto.webp') }}" alt="Gatto">
-                        </div>
-
-                        <div class="gatto-bg">
-                            QUESTO DIV AVRA' COME SFONDO IL GATTO
-                        </div>
-
-                        @yield('main-content')
-
-                    </div>
-
-                </div>
-
-            </div>
         </main>
 
         @include('partials.footer')
